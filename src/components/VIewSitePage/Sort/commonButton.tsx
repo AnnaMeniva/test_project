10e33a5e23d6eeb50f18ee71a403f537afd1da66
AnnaMeniva <annetmen@gmail.com>
2023-08-17 18:03:03 +0300

    creating-common-menu-and-button

---
+++ src/components/VIewSitePage/Sort/commonButton.tsx
@@ -0,0 +1,28 @@
+interface PropsType {
+  onClick: () => void;
+  content?: string;
+  image?: string;
+  altButton?: string;
+  buttonClassName?: string;
+  imageClassName?: string;
+}
+
+const CommonButton: React.FC<PropsType> = ({
+  onClick,
+  content,
+  imageClassName,
+  buttonClassName,
+  image,
+  altButton,
+}: PropsType) => {
+  return (
+    <>
+      <button className={buttonClassName} onClick={onClick}>
+        <img className={imageClassName} src={image} alt={altButton} />
+       Sort by: {content}
+      </button>
+    </>
+  );
+};
+
+export default CommonButton;
